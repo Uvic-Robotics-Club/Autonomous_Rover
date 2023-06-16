10ae3feed909abf8cf58a143f68ba785ce34b745
MyJetson <stevenlandriault@gmail.com>
2023-06-16 02:03:24 -0700

    two motors moving and working committing after succesful test'

--- src/motor_controller/scripts/motor_control/motor_control.ino
+++ src/motor_controller/scripts/motor_control/motor_control.ino
@@ -5,6 +5,10 @@
 #define IN2 9
 #define enB 7
 
+#define IN3 5
+#define IN4 4 //b
+#define enA 6 // enable
+
 ros::NodeHandle nh;
 
 void motorCb(const std_msgs::Int16& cmd_msg){
@@ -13,16 +17,29 @@ void motorCb(const std_msgs::Int16& cmd_msg){
     digitalWrite(enB, HIGH);
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
+
+    digitalWrite(enA, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    
   } else if(cmd_msg.data == -1){
     // Motor moves in reverse
     digitalWrite(enB, HIGH);
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
+    
+    digitalWrite(enA, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
   } else {
     // Motor stops
     digitalWrite(enB, LOW);
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
+
+    digitalWrite(enA, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
   }
 }
 
@@ -32,6 +49,11 @@ void setup(){
   pinMode(IN1, OUTPUT); 
   pinMode(IN2, OUTPUT);
   pinMode(enB, OUTPUT);
+
+  pinMode(IN3, OUTPUT); 
+  pinMode(IN4, OUTPUT);
+  pinMode(enA, OUTPUT);
+  
   nh.initNode();
   nh.subscribe(sub);
 }
